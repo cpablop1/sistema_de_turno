8cd5aded4b0118075c91e22ab426c4e3ff24e038
cpablop1 <cpablop1@gmail.com>
2024-05-14 14:02:01 -0600

    Estructura básica para el proyecto

---
+++ config/config.php
@@ -0,0 +1,6 @@
+<?php
+define('URL', 'http://localhost/proyecto-mvc/');
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'nombre_de_la_base_de_datos');
