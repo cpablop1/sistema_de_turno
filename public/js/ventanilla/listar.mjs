9e5b829bfc3be065cc8032046c4ef2994321af0d
cpablop1 <cpablop1@gmail.com>
2024-05-17 13:37:57 -0600

    Boceto de script para listar ventanilla agregada

--- public/js/ventanilla/listar.mjs
+++ public/js/ventanilla/listar.mjs
@@ -1,15 +1,38 @@
 import { host } from "../config.mjs";
-function crearVentanilla() {
-	fetch(`${host}/app/controllers/ventanilla/VentanillaController.php`, {
+function listarVentanilla() {
+	let ventanilla = document.getElementById('ventanilla');
+	ventanilla.innerHTML = '';
+	let card = '';
+	/* fetch(`${host}/app/controllers/ventanilla/VentanillaController.php`, {
 	})
-		.then(response => response.json())
-		.then(data => {
-			/** Procesar los datos **/
-			if (data.data) {
-				alert(data.msg);
-				bootstrap.Modal.getInstance(document.getElementById('mdl_crear_ventanilla')).hide();
-			}
-		});
+			.then(response => response.json())
+			.then(data => {
+					if (data.data) {
+							alert(data.msg);
+							bootstrap.Modal.getInstance(document.getElementById('mdl_crear_ventanilla')).hide();
+					}
+			}); */
+	for (let i = 0; i < 15; i++) {
+		let num = (i>=10) ? `<i class="bi bi-${i.toString().charAt(0)}-square-fill"><i class="bi bi-${i.toString().charAt(1)}-square-fill">` : `<i class="bi bi-${i+1}-square-fill">`;
+		console.log(num);
+		card += `
+            <div class="col">
+            <div class="card">
+              <div class="card-header">
+                <h3>
+                    <b>Ventanilla ${num}</b>
+                </h3>
+              </div>
+              <div class="card-body">
+                <h5 class="card-title">Special title treatment</h5>
+                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+              </div>
+            </div>
+        </div>
+            `;
+	}
+	ventanilla.innerHTML = card;
 }
 
-export default crearVentanilla;
+export default listarVentanilla;
--- public/js/ventanilla/main.js
+++ public/js/ventanilla/main.js
@@ -1,9 +1,14 @@
 import crearVentanilla from './crear.mjs';
+import listarVentanilla from './listar.mjs';
+window.onload = () => {
+	console.log('ñasjfñlsadjflk');
+	listarVentanilla();
+}
 
 document.getElementById('crear_ventanilla').addEventListener('click', () => {
-    new bootstrap.Modal(document.getElementById('mdl_crear_ventanilla')).show();
+	new bootstrap.Modal(document.getElementById('mdl_crear_ventanilla')).show();
 });
 
 document.getElementById('btn_crear_ventanilla').addEventListener('click', () => {
-    crearVentanilla();
+	crearVentanilla();
 });
